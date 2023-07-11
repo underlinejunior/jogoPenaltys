ca9c95719dda45e0773f0f0195aa52048fd1c966
Evaldo Júnior <41309801+underlinejunior@users.noreply.github.com>
2023-07-10 22:39:05 -0300

    mudança de rotina pra finalização do jogo

--- penalti.js
+++ penalti.js
@@ -1,20 +1,24 @@
+
+let regra = document.getElementById(`regra`);
+let title = document.getElementById(`title`);
+let bola = document.querySelector(`.bola`);
+let corpo = document.getElementById(`corpo`);
+let me = document.querySelector(`.me`);
+let md = document.querySelector(`.md`);
+let placar = document.getElementById(`placar`);
+
 function fimDeJogo() {
     document.getElementById("regra").style.color = "black";
-    document.getElementById("title").innerHTML = '';
-    if (placarJogador == placarMaquina) {
-        setTimeout(() => {
-            document.getElementById('regra').innerHTML = 'EMPATE!<br/>Vamos pros chutes alternados!';
-        }, 1000);
-        alternadas();
-    } else if (placarJogador > placarMaquina) {
-        document.getElementById('regra').innerHTML = 'FIM DE JOGO!<br/> Parabéns,você venceu!';
+    title = '';
+    if (placarJogador > placarMaquina) {
+        regra.innerHTML = 'FIM DE JOGO!<br/> Parabéns,você venceu!';
     } else {
-        document.getElementById('regra').innerHTML = 'FIM DE JOGO!<br/> Não era seu dia!';
+        regra.innerHTML = 'FIM DE JOGO!<br/> Não era seu dia!';
     }
     document.getElementById("bola").style.display = 'none';
-    document.getElementById('corpo').style.display = 'none';
-    Array.from(document.getElementsByClassName('me')).forEach(elem => elem.style.display = 'none');
-    Array.from(document.getElementsByClassName('md')).forEach(elem => elem.style.display = 'none');
+    corpo.style.display = 'none';
+    Array.from(me).forEach(elem => elem.style.display = 'none');
+    Array.from(md).forEach(elem => elem.style.display = 'none');
     document.getElementById("body").setAttribute('onKeyPress', '');
     for (let i = 1; i <= 5; i++) {
         document.getElementById(`position${i}`).style.display = "none";
@@ -31,20 +35,20 @@ let chutePlayer = 0,
     gol = 0;
 
 function chuteJogador(gol) {
-    document.getElementById('regra').innerHTML = "escolha onde chutar!"
+    regra.innerHTML = "escolha onde chutar!"
     chutePlayer = parseInt(gol);
     console.log("chute Jogador: " + chutePlayer);
     if (chutePlayer > 0 && chutePlayer < 6) {
         if (chutePlayer == 1) {
-            document.getElementById('bola').setAttribute('id', 'position1');
+            bola.setAttribute('id', 'position1');
         } else if (chutePlayer == 2) {
-            document.getElementById('bola').setAttribute('id', 'position2');
+            bola.setAttribute('id', 'position2');
         } else if (chutePlayer == 3) {
-            document.getElementById('bola').setAttribute('id', 'position3');
+            bola.setAttribute('id', 'position3');
         } else if (chutePlayer == 4) {
-            document.getElementById('bola').setAttribute('id', 'position4');
+            bola.setAttribute('id', 'position4');
         } else if (chutePlayer == 5) {
-            document.getElementById('bola').setAttribute('id', 'position5');
+            bola.setAttribute('id', 'position5');
         }
     }
     defesaMaquina();
@@ -56,20 +60,20 @@ function defesaMaquina() {
     defesaCPU = parseInt(localMaquina[maquina]);
     console.log("defesaCPU: " + defesaCPU);
 
-    document.getElementById("corpo").setAttribute('src', 'assets/goleiro.png');
-    document.querySelector(".me").setAttribute('src', 'assets/meGoleiro.png');
-    document.querySelector(".md").setAttribute('src', 'assets/mdGoleiro.png');
+    corpo.setAttribute('src', 'assets/goleiro.png');
+    me.setAttribute('src', 'assets/meGoleiro.png');
+    md.setAttribute('src', 'assets/mdGoleiro.png');
     if (defesaCPU == 1) {
-        document.querySelector(".me").setAttribute('id', 'position1');
+        me.setAttribute('id', 'position1');
     } else if (defesaCPU == 2) {
-        document.querySelector(".me").setAttribute('id', 'position2');
+        me.setAttribute('id', 'position2');
     } else if (defesaCPU == 3) {
-        document.querySelector(".md").setAttribute('id', 'position3');
+        md.setAttribute('id', 'position3');
     } else if (defesaCPU == 4) {
-        document.querySelector(".md").setAttribute('id', 'position4');
+        md.setAttribute('id', 'position4');
     } else if (defesaCPU == 5) {
-        document.querySelector(".me").setAttribute('id', 'position5');
-        document.querySelector(".md").setAttribute('id', 'position5');
+        me.setAttribute('id', 'position5');
+        md.setAttribute('id', 'position5');
     }
     apurarPlacarChuteJogador();
 }
@@ -80,20 +84,21 @@ function apurarPlacarChuteJogador() {
         mensagem = "Você fez GOL!";
         placarJogador++;
     } else if (chutePlayer === defesaCPU && chutePlayer > 0 && chutePlayer < 6) {
-        mensagem = "DEFENDEU!!";
+        mensagem = "CPU DEFENDEU!!";
     } else {
         mensagem = "PRA FORA!!";
     }
     setTimeout(() => {
-        document.getElementById("regra").innerHTML = mensagem;
-        document.getElementById("placar").innerHTML = `Player:${placarJogador} <br/> Maquina:${placarMaquina}`;
+        console.log(mensagem);
+        regra.innerHTML = mensagem;
+        placar.innerHTML = `Player:${placarJogador} <br/> Maquina:${placarMaquina}`;
     }, 1500);
     setTimeout(() => {
-        document.getElementById("regra").innerHTML = "Escolha onde acha que vai ser o chute";
-        document.querySelector(`.bola`).setAttribute('src', 'assets/bola.png');
-        document.querySelector(`.bola`).setAttribute('id', 'bola');
-        document.querySelector(`.me`).setAttribute('id', 'position01');
-        document.querySelector(`.md`).setAttribute('id', 'position02');
+        regra.innerHTML = "Escolha onde acha que vai ser o chute";
+        bola.setAttribute('src', 'assets/bola.png');
+        bola.setAttribute('id', 'bola');
+        me.setAttribute('id', 'position01');
+        md.setAttribute('id', 'position02');
     }, 1500);
 }
 
@@ -105,15 +110,15 @@ function chuteMaquina(gol) {
     console.log("chuteCPU: " + chuteCPU);
     if (chuteCPU > 0 && chuteCPU < 6) {
         if (chuteCPU == 1) {
-            document.getElementById('bola').setAttribute('id', 'position1');
+            bola.setAttribute('id', 'position1');
         } else if (chuteCPU == 2) {
-            document.getElementById('bola').setAttribute('id', 'position2');
+            bola.setAttribute('id', 'position2');
         } else if (chuteCPU == 3) {
-            document.getElementById('bola').setAttribute('id', 'position3');
+            bola.setAttribute('id', 'position3');
         } else if (chuteCPU == 4) {
-            document.getElementById('bola').setAttribute('id', 'position4');
+            bola.setAttribute('id', 'position4');
         } else if (chuteCPU == 5) {
-            document.getElementById('bola').setAttribute('id', 'position5');
+            bola.setAttribute('id', 'position5');
         }
 
         setTimeout(() => {
@@ -127,21 +132,21 @@ function defesaJogador(gol) {
     let chute = "position" + gol;
     defesaPlayer = parseInt(gol);
     console.log("defesaJogador:" + defesaPlayer);
-    document.getElementById("corpo").setAttribute('src', 'assets/goleiro2.png');
-    document.querySelector(".me").setAttribute('src', 'assets/meGoleiro2.png');
-    document.querySelector(".md").setAttribute('src', 'assets/mdGoleiro2.png');
+    corpo.setAttribute('src', 'assets/goleiro2.png');
+    me.setAttribute('src', 'assets/meGoleiro2.png');
+    md.setAttribute('src', 'assets/mdGoleiro2.png');
 
     if (defesaPlayer == 1) {
-        document.querySelector(".me").setAttribute('id', 'position1');
+        me.setAttribute('id', 'position1');
     } else if (defesaPlayer == 2) {
-        document.querySelector(".me").setAttribute('id', 'position2');
+        me.setAttribute('id', 'position2');
     } else if (defesaPlayer == 3) {
-        document.querySelector(".md").setAttribute('id', 'position3');
+        md.setAttribute('id', 'position3');
     } else if (defesaPlayer == 4) {
-        document.querySelector(".md").setAttribute('id', 'position4');
+        md.setAttribute('id', 'position4');
     } else if (defesaPlayer == 5) {
-        document.querySelector(".me").setAttribute('id', 'position5');
-        document.querySelector(".md").setAttribute('id', 'position5');
+        me.setAttribute('id', 'position5');
+        md.setAttribute('id', 'position5');
     }
 
     setTimeout(() => {
@@ -155,48 +160,43 @@ function apurarPlacarChuteMaquina() {
         mensagem = "GOL do adversário!";
         placarMaquina++;
     } else if (chuteCPU === defesaPlayer && chuteCPU > 0 && chuteCPU < 6) {
-        mensagem = "DEFENDEU!!";
+        mensagem = "VOCÊ DEFENDEU!!";
     } else {
         mensagem = "PRA FORA!!";
     }
-    document.getElementById("regra").innerHTML = mensagem;
-    document.getElementById("placar").innerHTML = `Player:${placarJogador} <br/> Maquina:${placarMaquina}`;
+
+    console.log(mensagem);
+    regra.innerHTML = mensagem;
+    placar.innerHTML = `Player:${placarJogador} <br/> Maquina:${placarMaquina}`;
 
     setTimeout(() => {
-        document.getElementById("regra").innerHTML = "Escolha onde deseja chutar";
-        document.querySelector(`.bola`).setAttribute('src', 'assets/bola.png');
-        document.querySelector(`.bola`).setAttribute('id', 'bola');
-        document.querySelector(`.me`).setAttribute('id', 'position01');
-        document.querySelector(`.md`).setAttribute('id', 'position02');
+        regra.innerHTML = "Escolha onde deseja chutar";
+        bola.setAttribute('src', 'assets/bola.png');
+        bola.setAttribute('id', 'bola');
+        me.setAttribute('id', 'position01');
+        md.setAttribute('id', 'position02');
     }, 1500);
 }
 
-
-
 function tecla_pressionada(event) {
     let tecla = event.which || event.keyCode;
-    let meElements = document.querySelectorAll('.me');
-    let mdElements = document.querySelectorAll('.md');
-    document.querySelector('.me').setAttribute('id', 'position01');
-    document.querySelector('.md').setAttribute('id', 'position02');
+    me.setAttribute('id', 'position01');
+    md.setAttribute('id', 'position02');
 
     if (event.key === 'Enter' && contador === 0) {
         contador++;
-        document.getElementById('regra').style.color = 'black';
-        document.getElementById('title').innerHTML = '';
-        document.getElementById('regra').innerHTML = 'Escolha onde deseja chutar';
-    } else {
-        document.getElementById('regra').style.color = 'red';
-    }
+        regra.style.color = 'black';
+        title = '';
+        regra.innerHTML = 'Escolha onde deseja chutar';
 
-    if (tecla >= 49 && tecla <= 53 && contador <= 10) {
+    } else if ((tecla >= 49 && tecla <= 53 )&& contador>0 &&(placarJogador - placarMaquina<2 || placarMaquina-placarJogador<2)) {
         gol = parseInt(event.key);
         contador++;
 
         let mensagem = (contador % 2 !== 0) ? 'Escolha onde deseja chutar' : 'Escolha onde acha que vai ser o chute';
-        document.getElementById('regra').style.color = 'black';
-        document.getElementById('title').innerHTML = '';
-        document.getElementById('regra').innerHTML = mensagem;
+        regra.style.color = 'black';
+        title = '';
+        regra.innerHTML = mensagem;
 
         if (contador % 2 !== 0) {
             chuteJogador(gol);
@@ -204,50 +204,22 @@ function tecla_pressionada(event) {
             chuteMaquina(gol);
         }
 
-        if (contador > 10) {
-            document.getElementById('regra').style.display = 'absolute';
-            document.getElementById('regra').style.top = '18vw';
-            document.getElementById('regra').style.left = '0vw';
+        if (contador<10 &&(placarJogador - placarMaquina>2 || placarMaquina-placarJogador>2)) {
+            regra.style.display = 'absolute';
+            regra.style.top = '18vw';
+            regra.style.left = '0vw';
             setTimeout(fimDeJogo, 1500);
         }
-    } else if ((tecla < 49 || tecla > 53)&&event.key !== 'Enter') {
-        setTimeout(() => {
-            document.getElementById('regra').innerHTML = 'PRA FORA!';
-        }, 1000)
-    }
-
-    function alternadas() {
-        placarJogador = 0;
-        placarMaquina = 0;
-        contador = 0;
-        if (tecla >= 49 && tecla <= 53) {
-            gol = parseInt(event.key);
-
-            let mensagem = (contador % 2 !== 0) ? 'Escolha onde deseja chutar' : 'Escolha onde acha que vai ser o chute';
-            document.getElementById('regra').style.color = 'black';
-            document.getElementById('title').innerHTML = '';
-            document.getElementById('regra').innerHTML = mensagem;
-            
-
-            if (contador % 2 !== 0) {
-                chuteJogador(gol);
-                contador++;
-            } else {
-                chuteMaquina(gol);
-                contador++
-            }
-
-            if (placarJogador == placarMaquina + 2 || placarMaquina == placarJogador + 2) {
-                document.getElementById('regra').style.display = 'absolute';
-                document.getElementById('regra').style.top = '18vw';
-                document.getElementById('regra').style.left = '0vw';
-                setTimeout(fimDeJogo, 1500);
-            }
-        } else if (tecla < 49 || tecla > 53) {
-            setTimeout(() => {
-                document.getElementById('regra').innerHTML = 'PRA FORA!';
-            }, 1000)
+        if (contador>=10 &&(placarJogador - placarMaquina>1 || placarMaquina-placarJogador>1)) {
+            regra.style.display = 'absolute';
+            regra.style.top = '18vw';
+            regra.style.left = '0vw';
+            setTimeout(fimDeJogo, 1500);
         }
+    } else if ((tecla < 49 || tecla > 53) && event.key !== 'Enter') {
+        setTimeout(() => {
+            regra.innerHTML = 'PRA FORA!';
+        }, 1000)
     }
 }
 
